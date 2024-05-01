0c27a887fcbb721ab2a6ce822b1c35d354e895cf
Brijesh Vadgama <brijuvadgama@gmail.com>
2024-05-01 19:33:09 +0530

    commit

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -1,17 +1,10 @@
 const mongoose = require("mongoose");
+
 const StudentSchema = mongoose.Schema({
-  StudentId: {
-    type: Number,
-    unique: true,
-  },
-  StudentName: {
-    type: String,
-    required: true,
-  },
-  StudentAddress: {
-    type: String,
-    required: true,
-  },
+  StudentId: Number,
+  StudentName: String,
+  StudentAddress: String,
 });
+
 const Student = mongoose.model("Student", StudentSchema);
 module.exports = Student;
